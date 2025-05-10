72eac8aff5338c118ef6aa55d38c5c807a977c9f
Irika271 <Semakovais@yandex.ru>
2025-05-10 13:03:39 +0300

    Update Character.js

--- src/js/Character.js
+++ src/js/Character.js
@@ -12,24 +12,23 @@ class Character {
         return this._stoned;
     }
 
-    
-   
+    set attack(value) {
+        this._attack = value;
     }
 
     get attack() {
-       let attack = this.baseAttack;
+       return this.baseAttack;
+    }
+    
+  calculateAttack(distance) {
+        this.distance = distance;
+        let attack = this.baseAttack;
 
-        // Уменьшение атаки в зависимости от расстояния
-        const linearReduction = Math.max(0, 1 - (this.distance - 1) * 0.1);
+         const linearReduction = Math.max(0, 1 - (distance - 1) * 0.1);
         attack *= linearReduction;
 
-        // Если персонаж "stoned", уменьшаем атаку на логарифм
-        if (this.stoned) {
-            attack -= Math.log2(this.distance) * 5; // Пример, как можно уменьшить атаку
-            attack = Math.max(0, attack); // Убедимся, что атака не отрицательная
-        }
-
         return attack;
+    }
 }
 
-module.exports = Character; 
+module.exports = Character;
